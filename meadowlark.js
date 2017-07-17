8c4bff73f18d22bfee72c681d2e9739d1675dc90
konstaintine811 <galystyan777@gmail.com>
2017-07-17 19:10:25 +0300

    Node-practic

---
+++ meadowlark.js
@@ -0,0 +1,53 @@
+/*
+* @Author: Admin
+* @Date:   2017-07-17 17:49:44
+* @Last Modified by:   Admin
+* @Last Modified time: 2017-07-17 18:50:13
+*/
+
+'use strict';
+
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars')
+	.create({defaultLayout: 'main'});
+var fortunes = [
+	"Победи свои страхи, или они победят тебя.",
+	"Рекам нужны стоки.",
+	"Не бойся неведомого.",
+	"Тебя ждет приятный сюрприз.",
+	"Будь проще везде, где только можно."];
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	var randomFortune = 
+		fortunes[Math.floor(Math.random() * fortunes.length)];
+
+	res.render('about', { fortune: randomFortune });
+});
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Express запущен на http://localhost:' +
+		app.get('port') + '; нажмите Ctrl+C для завершения.');
+});
